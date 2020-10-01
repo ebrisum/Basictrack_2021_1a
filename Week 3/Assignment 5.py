0dca4ee53e640e6d75a964d264ff17db36b34a7b
Warner <warnerherrema@gmail.com>
2020-10-01 17:47:13 +0200

    Assignment week 3

--- Week 3/Assignment 5.py
+++ Week 3/Assignment 5.py
@@ -4,23 +4,13 @@ from datetime import datetime
 now = datetime.now()
 hour = int(now.strftime("%H"))
 minute = int(now.strftime("%M"))
-
-print(hour)
-print(minute)
 window = turtle.Screen()
 warner = turtle.Turtle()
+move = 30
 
 warner.speed(100)
 warner.shape("turtle")
-warner.penup()
-warner.forward(250)
-warner.pendown()
-warner.forward(10)
-warner.penup()
-warner.forward(30)
-warner.stamp()
-warner.backward(290)
-move=30
+
 for i in range(12):
     warner.penup()
     warner.forward(250)
@@ -33,8 +23,6 @@ for i in range(12):
     warner.right(move)
     move=move+30
 
-warner.right(30)
-warner.speed(5)
 warner.pendown()
 warner.pencolor("red")
 warner.right(30*hour-90)
@@ -42,12 +30,11 @@ warner.pensize(10)
 warner.forward(190)
 warner.backward(190)
 warner.left(30*hour-60)
-warner.right(6*minute-90)
+warner.right(6*minute-60)
 warner.pensize(5)
 warner.forward(240)
 warner.backward(240)
 
 
-turtle.mainloop()
 window.exitonclick()
 
