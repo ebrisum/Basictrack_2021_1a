6b2cf4f4a332eae461c68bbeb8f5897ff17bb851
Warner <warnerherrema@gmail.com>
2020-10-01 15:08:37 +0200

    Assignment week 3

---
+++ Week 3/Assignment 4.py
@@ -0,0 +1,19 @@
+numbers = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+for i in numbers:
+    print(i)
+
+for i in numbers:
+    print(i, i**0.5)
+
+total = 0
+for i in numbers:
+    total+=i
+print(total)
+
+total = 1
+for x in numbers:
+    total= total * i
+print(total)
+
+
